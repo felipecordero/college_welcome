12ff0c08dd2b3cd328e23984d08250c5dfb8307c
felipecordero <correofelipecordero@gmail.com>
2024-08-25 21:15:01 -0400

    logo fix

--- horarie.py
+++ horarie.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import pandas as pd
 st.set_page_config(layout="wide",)
-st.markdown(" <style> div[class^='block-container'] { padding-top: 2rem; } </style> ", unsafe_allow_html=True)
+# st.markdown(" <style> div[class^='block-container'] { padding-top: 2rem; } </style> ", unsafe_allow_html=True)
 df = pd.read_csv("horaire.csv", sep=";")
 df = df.replace("Drishti kaushik", "Drishti Kaushik")
 df = df.replace("Edaurdo Cuellar", "Eduardo Cuellar")
